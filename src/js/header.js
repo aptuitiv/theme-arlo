881d2a499e0ee2b421751e2bf1253853bf153efa
eharringtonaptuitiv <eharrington@aptuitiv.com>
2025-04-28 15:58:39 -0400

    working on tasks to get site done. Working on sticky header

--- src/js/header.js
+++ src/js/header.js
@@ -1,30 +1,80 @@
-/**
- * Handle all functionality pertaining to the sticky header
- */
 const stickyHeader = { // eslint-disable-line no-unused-vars
+    lastScrollY: 0,
+    header: null,
+
     init() {
-        this.setupIntersectionObserver();
+        this.header = document.querySelector(".HeaderWrap");
+
+        if (!this.header) {
+            console.error("HeaderWrap not found!");
+            return;
+        }
+
+        // this.setupIntersectionObserver();
+        this.setupScrollListener();
     },
 
-    /**
-     * Detects when the sticky header is scrolled to show a box shadow on it
-     */
-    setupIntersectionObserver: () => {
-        const headerEl = document.querySelector('.js-stickyHeader');
-        const boundaryEl = document.querySelector('.js-stickyHeaderBoundary');
+    // setupIntersectionObserver() {
+    //     const headerEl = document.querySelector('.js-stickyHeader');
+    //     const boundaryEl = document.querySelector('.js-stickyHeaderBoundary');
 
-        const handler = (entries) => {
-            if (!entries[0].isIntersecting) {
-                headerEl.classList.add('is-sticky');
-            } else {
-                headerEl.classList.remove('is-sticky');
-            }
-        };
+    //     if (!headerEl || !boundaryEl) {
+    //         console.error('StickyHeader or Boundary not found!');
+    //         return;
+    //     }
 
-        const headerObserver = new IntersectionObserver(handler, {
-            rootMargin: '0px 0px -100% 0px' // or experiment with -1px if that’s enough
+    //     const handler = (entries) => {
+    //         if (!entries[0].isIntersecting) {
+    //             headerEl.classList.add('is-sticky');
+    //         } else {
+    //             headerEl.classList.remove('is-sticky');
+    //         }
+    //     };
+
+    //     const observer = new IntersectionObserver(handler, {
+    //         rootMargin: '0px 0px -100% 0px'
+    //     });
+
+    //     observer.observe(boundaryEl);
+    // },
+
+    setupScrollListener() {
+        window.addEventListener("scroll", () => {
+            requestAnimationFrame(this.handleScroll.bind(this));
         });
 
-        headerObserver.observe(boundaryEl);
+        window.addEventListener("resize", () => {
+            this.handleResize();
+        });
     },
+
+    handleScroll() {
+        const currentScrollY = window.scrollY;
+
+        if (window.innerWidth > 800) {
+            this.header.classList.remove("is-hidden");
+            return;
+        }
+
+        if (currentScrollY > this.lastScrollY) {
+            // Scroll down
+            this.header.classList.add("is-hidden");
+        } else {
+            // Scroll up
+            this.header.classList.remove("is-hidden");
+        }
+
+        this.lastScrollY = currentScrollY;
+    },
+
+    handleResize() {
+        if (window.innerWidth > 800) {
+            this.header.classList.remove("is-hidden");
+        }
+    }
 };
+
+// Then don't forget to call stickyHeader.init() somewhere after your DOM is ready:
+document.addEventListener('DOMContentLoaded', () => {
+    stickyHeader.init();
+});

8f96dfa42a76bdf712a8325b9b3978d1ecd89281
Eric Tompkins <eric@aptuitiv.com>
2026-08-13 20:54:51 -0400

    Added support for an inobtrusive "stop" button on videos that autoplay and have controls hidden.
    This also handles the user's "prefers-reduced-motion: reduce" choice and pauses autoplaying videos.
    This is all done for accessibility reasons

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ import accordion from './accordion';
 import { setCookie } from './lib/cookie';
 import popOutMenu from './navigation/pop-out-menu';
 import navAccessibility from './navigation/accessibility';
+import video from './video';
 
 /* global MicroModal:readonly notificationBarCookieExpirationDuration */
 
@@ -21,6 +22,7 @@ const main = {
         accordion.init();
         popOutMenu.init();
         navAccessibility.init();
+        video.init();
         MicroModal.init({
             disableFocus: true,
         });
